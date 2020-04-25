0e9923319ab81d133fb3e02c195692fff4880a04
nifal <nifaladam@gmail.com>
2020-04-26 00:07:13 +0530

    init create-react-library@3.0.8

---
+++ example/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import { ExampleComponent } from 'materialui-table'
+import 'materialui-table/dist/index.css'
+
+const App = () => {
+  return <ExampleComponent text="Create React Library Example 😄" />
+}
+
+export default App
